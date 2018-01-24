6ecc4675dfe2d2b5992c59051b76f8a7be58c4ad
BigOceans <jxfanzhihai@163.com>
2018-01-24 21:43:09 +0800

    Update load_image.py

--- load_image.py
+++ load_image.py
@@ -6,8 +6,15 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+# 以灰度模式读入图片
 img = cv2.imread('football.jpg',cv2.IMREAD_GRAYSCALE)
 
+# 展示图片
 cv2.imshow('image',img)
+# 等待键盘键入
 cv2.waitKey(0)
+# 删除建立的窗口
 cv2.destroyAllWindows()
+
+# 保存图片
+cv2.imwrite('balls',img)
