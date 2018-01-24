e2c979b67d56d9ef8dff7cc0de2831153aaa5080
BigOceans <jxfanzhihai@163.com>
2018-01-24 21:27:37 +0800

    Add files via upload

---
+++ load_image.py
@@ -0,0 +1,13 @@
+#coding:utf-8
+"""
+@auther:BigOceans
+"""
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+img = cv2.imread('football.jpg',cv2.IMREAD_GRAYSCALE)
+
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
