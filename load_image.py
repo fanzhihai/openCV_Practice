8bcdf8f27b66b50360f2e71dc9e8fdee13b18eda
BigOceans <jxfanzhihai@163.com>
2018-01-24 21:44:00 +0800

    Update load_image.py

--- load_image.py
+++ load_image.py
@@ -13,8 +13,9 @@ img = cv2.imread('football.jpg',cv2.IMREAD_GRAYSCALE)
 cv2.imshow('image',img)
 # 等待键盘键入
 cv2.waitKey(0)
-# 删除建立的窗口
-cv2.destroyAllWindows()
 
 # 保存图片
 cv2.imwrite('balls',img)
+
+# 删除建立的窗口
+cv2.destroyAllWindows()
